da5e2c538417997827c0b29a3bb0c26f3332334d
RugbyGuy01 <goodmail@golfpvcc.com>
2024-03-06 08:40:35 -0500

    Rev 4 of team scores

---
+++ app/src/main/java/com/golfpvcc/teamscore_rev4/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.golfpvcc.teamscore_rev4
+/*
+Code is model after the you tube view Android Jetpack Compose Notes app with Photos
+https://www.youtube.com/watch?v=FJZ__Ri38I0
+ */
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.golfpvcc.teamscore_rev4.ui.theme.TeamScore_Rev4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TeamScore_Rev4Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
